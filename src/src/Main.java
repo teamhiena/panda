57da073f9698f868faa7087dcf8c56f7c78e612b
Miklós Beregszászi <beregmiklos@sch.bme.hu>
2019-03-18 23:50:00 +0100

    src mappa+Main

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Istenkirály vagy Miki, hogy ezt kipróbáltad!");
-    }}
+        Menu mainMenu = new Menu();
+        Scanner scanner = new Scanner(System.in);
+        int chosenUseCase = 0;
+
+        while (chosenUseCase != 15) {
+            mainMenu.show();
+            chosenUseCase = Integer.parseInt(scanner.nextLine());
+            System.out.println(chosenUseCase);
+        }
+    }
+}
---
+++ src/src/Menu.java
@@ -0,0 +1,24 @@
+public class Menu {
+    public void show(){
+        System.out.println("Az alábbi use-case-ekből választhat:");
+        System.out.println("1. Orangutan steps on Tile");
+        System.out.println("2. Orangutan steps on WeakTile");
+        System.out.println("3. Orangutan steps on BrokenTile");
+        System.out.println("4. Orangutan cannot enter Tile");
+        System.out.println("5. Orangutan catches its first panda");
+        System.out.println("6. Orangutan catches panda, while it already has panda");
+        System.out.println("7. Orangutan enters wardrobe");
+        System.out.println("8. Orangutan won");
+        System.out.println("9. Orangutan takes the exit with panda(s)");
+        System.out.println("10. TiredPanda enters fotel");
+        System.out.println("11. DiabeticPanda gets frightened");
+        System.out.println("12. Panda enters wardrobe");
+        System.out.println("13. Panda in row steps on broken tile");
+        System.out.println("14. Panda in freeroam steps on BrokenTile");
+        System.out.println("15. Exit application");
+        System.out.print("A választott use-case: ");
+    }
+    public void manageUseCase(int choseUseCase){
+
+    }
+}
