68b7151e315c1c3344b50490e1922bf5ed32fb26
Miklós Beregszászi <beregmiklos@sch.bme.hu>
2019-03-18 20:52:58 +0100

    src mappa+Main

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Istenkirály vagy Miki, hogy ezt kipróbáltad.");
+        System.out.println("Istenkirály vagy Miki, hogy ezt kipróbáltad!");
     }}
