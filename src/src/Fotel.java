8346d1f256fd321312f1ff35707d97b58a053317
Miklós Beregszászi <beregmiklos@sch.bme.hu>
2019-03-25 12:10:40 +0100

    kommenezgetesi

--- src/src/Fotel.java
+++ src/src/Fotel.java
@@ -1,29 +1,54 @@
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * A fotelt megvalosito osztaly. Ha egy faradekony panda elhalad mellette, akkor leul.
+ */
 public class Fotel extends Entity{
-	private Tile enteredFrom=null;
-	private long timeLeft=100; //vitathato
-	
+	private Tile enteredFrom=null; //Errol a mezorol lehet belepni a fotelre.
+	private long timeLeft=100; //Ennyi ido van meg hatra.(eddig ul meg ott a panda)
 
+	//METODUSOK
+
+	/**
+	 * Egy orangutan megprobal belepni a fotelbe. Ez termeszetesen nem lehetseges.
+	 * @param o
+	 * @return
+	 */
 	public boolean stepIn(Orangutan o) {
 		ArrayList<Object> par = new ArrayList<>(); par.add(o);
 		Logger.enter(this, "stepIn", par);
 		Logger.exit(this, "stepIn", false);
 		return false;
-	}	
+	}
+
+	/**
+	 * Egy panda megprobal belepni a fotelbe.
+	 * @param p
+	 * @return
+	 */
 	public boolean stepIn(Panda p) {
-		if (tile.getAnimal()!=null) return false; //valszeg nem kell de just to make sure
+		if (tile.getAnimal()!=null) return false; //Nem valoszinu, hogy szukseges.
 		enteredFrom=p.getTile();
 		resetTimeLeft();
 		p.affectedBy(this);
 		return true;		
 	}
-	
+
+	/**
+	 * Megmondja, hogy van-e valaki a fotelban.
+	 */
 	public boolean isEmpty() {
-		return tile.getAnimal()==null;
+		Logger.enter(this, "isEmpty", new ArrayList<>());
+		boolean ret = false;
+		if(tile.getAnimal() == null) ret = true;
+		Logger.exit(this, "isEmpty", ret);
+		return ret;
 	}
-	
+
+	/**
+	 * Csokkenti a hatra levo idot.
+	 */
 	public void decrTimeLeft() {
 		timeLeft--;
 	}
